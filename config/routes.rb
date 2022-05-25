a4b7ae54eef2736ef35474b740ba8a9f64e51681
Stephen Postma <stephensebastiaanpostma@gmail.com>
2022-05-25 15:39:23 +0200

    refactoring done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "restaurants", to: "restaurants#index"
-  get "restaurants/new", to: "restaurants#new"
-  get "restaurants/:id", to: "restaurants#show", as: :restaurant
-  post "restaurants", to: "restaurants#create"
   resources :restaurants do
-    resources :reviews, only: %i[new create]
+    resources :reviews, only: %i[create]
   end
 end
