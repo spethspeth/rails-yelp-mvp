be761b1491bd7c29a5c2ea5e8751cfe07eda670d
Stephen Postma <stephensebastiaanpostma@gmail.com>
2022-05-25 13:07:17 +0200

    green rake

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "restaurants", to: "restaurants#index"
+  get "restaurants/new", to: "restaurants#new"
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  post "restaurants", to: "restaurants#create"
+  resources :restaurants do
+    resources :reviews, only: %i[new create]
+  end
 end
